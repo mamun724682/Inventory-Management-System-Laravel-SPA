3ed6e15ca1f356393ffbb5ceac0b7c066cf98376
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-09-25 20:28:02 +0600

    Employee migration file added

---
+++ database/migrations/2020_09_25_202239_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->float('salary');
+            $table->string('photo')->nullable();
+            $table->string('nid')->nullable();
+            $table->string('joining_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
