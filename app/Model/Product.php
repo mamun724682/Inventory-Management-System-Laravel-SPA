bdd1baa45ebd2a33feb04a8a313805c187f2f0f0
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-09-30 14:48:58 +0600

    Add product model, migration and controller

---
+++ app/Model/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+    	'category_id', 'supplier_id','product_name','product_code'),'root','buying_price','selling_price','buying_date','image','product_quantity');
+    ];
+}
---
+++ database/migrations/2020_09_30_144042_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('category_id');
+            $table->integer('supplier_id')->nullable();
+
+            $table->string('product_name');
+            $table->string('product_code')->nullable();
+            $table->string('root')->nullable();
+            $table->string('buying_price')->nullable();
+            $table->string('selling_price');
+            $table->string('buying_date')->nullable();
+            $table->string('image')->nullable();
+            $table->string('product_quantity');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
